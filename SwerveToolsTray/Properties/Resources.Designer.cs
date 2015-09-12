0288608dffcae5fe7a3b27629e8768f78068e69d
Robert Atkinson <bob@theatkinsons.org>
2015-09-12 14:07:46 -0700

    massive rework of build configurations

--- SwerveToolsTray/Properties/Resources.Designer.cs
+++ SwerveToolsTray/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SwerveToolsTray.Properties
-    {
-
-
+namespace SwerveToolsTray.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,50 +22,42 @@ namespace SwerveToolsTray.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-        {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-            {
-            }
-
+        internal Resources() {
+        }
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-            {
-            get
-                {
-                if ((resourceMan == null))
-                    {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SwerveToolsTray.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
-                    }
-                return resourceMan;
                 }
+                return resourceMan;
             }
-
+        }
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-            {
-            get
-                {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
-                }
-            set
-                {
+            }
+            set {
                 resourceCulture = value;
-                }
             }
         }
     }
+}
