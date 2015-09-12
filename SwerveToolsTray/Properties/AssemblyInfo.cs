6f5ab4265a244c6326279a77b4811d4269c286b0
RobertAtkinson <bob@theatkinsons.org>
2015-09-12 16:48:59 -0700

    more setup work

--- SwerveToolsTray/Properties/AssemblyInfo.cs
+++ SwerveToolsTray/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SwerveToolsTray")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("System tray icon for Swerve Robotics Tools Suite")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SwerveToolsTray")]
+[assembly: AssemblyCompany("Swerve Robotics")]
+[assembly: AssemblyProduct("Swerve Tools Suite")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.*")]
+//[assembly: AssemblyFileVersion("0.5.0.0")]
