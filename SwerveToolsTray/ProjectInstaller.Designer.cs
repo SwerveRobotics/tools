2ec736d4153d248d05f5e61af2daf90044530bd5
RobertAtkinson <bob@theatkinsons.org>
2015-09-13 09:54:28 -0700

    add installer to tray

---
+++ SwerveToolsTray/ProjectInstaller.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Org.SwerveRobotics.Tools.SwerveToolsTray
+    {
+    partial class ProjectInstaller
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+            {
+            if (disposing && (components != null))
+                {
+                components.Dispose();
+                }
+            base.Dispose(disposing);
+            }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+            {
+            // 
+            // ProjectInstaller
+            // 
+            this.Committed += new System.Configuration.Install.InstallEventHandler(this.ProjectInstaller_Committed);
+            this.AfterInstall += new System.Configuration.Install.InstallEventHandler(this.ProjectInstaller_AfterInstall);
+            this.AfterRollback += new System.Configuration.Install.InstallEventHandler(this.ProjectInstaller_AfterRollback);
+            this.AfterUninstall += new System.Configuration.Install.InstallEventHandler(this.ProjectInstaller_AfterUninstall);
+            this.Committing += new System.Configuration.Install.InstallEventHandler(this.ProjectInstaller_Committing);
+            this.BeforeInstall += new System.Configuration.Install.InstallEventHandler(this.ProjectInstaller_BeforeInstall);
+            this.BeforeRollback += new System.Configuration.Install.InstallEventHandler(this.ProjectInstaller_BeforeRollback);
+            this.BeforeUninstall += new System.Configuration.Install.InstallEventHandler(this.ProjectInstaller_BeforeUninstall);
+
+            }
+
+        #endregion
+        }
+    }
---
+++ SwerveToolsTray/ProjectInstaller.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Org.SwerveRobotics.Tools.SwerveToolsTray
+    {
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+        {
+        public ProjectInstaller()
+            {
+            InitializeComponent();
+            }
+
+        private void ProjectInstaller_BeforeInstall(object sender, InstallEventArgs e)
+            {
+            Trace("before install");
+            }
+
+        private void ProjectInstaller_AfterInstall(object sender, InstallEventArgs e)
+            {
+            Trace("after install");
+            }
+
+        private void ProjectInstaller_BeforeUninstall(object sender, InstallEventArgs e)
+            {
+            Trace("before uninstall");
+            }
+
+        private void ProjectInstaller_AfterUninstall(object sender, InstallEventArgs e)
+            {
+            Trace("after uninstall");
+            }
+
+        private void ProjectInstaller_Committing(object sender, InstallEventArgs e)
+            {
+            Trace("committing");
+            }
+
+        private void ProjectInstaller_Committed(object sender, InstallEventArgs e)
+            {
+            Trace("committed");
+            }
+
+        private void ProjectInstaller_BeforeRollback(object sender, InstallEventArgs e)
+            {
+            Trace("before rollback");
+            }
+
+        private void ProjectInstaller_AfterRollback(object sender, InstallEventArgs e)
+            {
+            Trace("after rollback");
+            }
+
+        //--------------------------------------------------------------------------------------
+        // Utility
+        //--------------------------------------------------------------------------------------
+       
+        void StartApplication()
+            {
+            Trace("starting application...");
+            Trace("...started");
+            }
+
+        void StopApplication()
+            {
+            Trace("stopping application...");
+            Trace("...started");
+            }
+
+        void Trace(string message)
+            {
+            System.Diagnostics.Trace.WriteLine($"BotBug: SwerveToolsTray: installer: {message}");
+            }
+
+        }
+    }
