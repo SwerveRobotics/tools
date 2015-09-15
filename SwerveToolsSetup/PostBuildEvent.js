ab338d197383addf8e97cb5b3290c948b15ea92b
RobertAtkinson <bob@theatkinsons.org>
2015-09-15 11:10:07 -0700

    disable modify and repair in add-remove programs

--- SwerveToolsSetup/PostBuildEvent.js
+++ SwerveToolsSetup/PostBuildEvent.js
@@ -5,18 +5,18 @@
 // Constant values from Windows Installer
 var msiOpenDatabaseModeTransact = 1;
 
-var msiViewModifyInsert         = 1
-var msiViewModifyUpdate         = 2
-var msiViewModifyAssign         = 3
-var msiViewModifyReplace        = 4
-var msiViewModifyDelete         = 6
+var msiViewModifyInsert = 1;
+var msiViewModifyUpdate = 2;
+var msiViewModifyAssign = 3;
+var msiViewModifyReplace = 4;
+var msiViewModifyDelete = 6;
 
 var msidbCustomActionTypeRollback       = 0x00000100;
 var msidbCustomActionTypeCommit         = 0x00000200;
 var msidbCustomActionTypeInScript       = 0x00000400;
 var msidbCustomActionTypeNoImpersonate  = 0x00000800;
 
-if (WScript.Arguments.Length != 1)
+if (WScript.Arguments.Length !== 1)
     {
 	WScript.StdErr.WriteLine(WScript.ScriptName + " file");
 	WScript.Quit(1);
@@ -26,47 +26,31 @@ var filespec = WScript.Arguments(0);
 
 WScript.StdErr.WriteLine(WScript.ScriptName + ": processing '" + filespec + "'");
 
+// Documentation on WindowsInstaller.Installer interface
+// https://msdn.microsoft.com/en-us/library/aa369432(v=vs.85).aspx
 var installer = WScript.CreateObject("WindowsInstaller.Installer");
 var database = installer.OpenDatabase(filespec, msiOpenDatabaseModeTransact);
 
-var sql
-var view
-var record
-var action
-var type
-var source
-var target
-var custact
-var noimp
-
 try {
-	sql = "SELECT `Action`, `Type`, `Source`, `Target` FROM `CustomAction`";
-	view = database.OpenView(sql);
-	view.Execute();
-	record = view.Fetch();
+    // SQL syntax
+    // https://msdn.microsoft.com/en-us/library/aa372021(v=vs.85).aspx
+    // https://msdn.microsoft.com/en-us/library/aa368562(v=vs.85).aspx
+    // https://github.com/Excel-DNA/WiXInstaller/blob/master/Source/WiRunSQL.vbs
     
-	while (record) {
-	    action = record.StringData(1);
-        type = record.IntegerData(2);
-        source = record.StringData(3);
-        target = record.StringData(4);
+    // Disable both the modify and repair options in Add/Remove programs
+    var view;
+    view = database.OpenView("INSERT INTO `Property` (`Property`.`Property`, `Property`.`Value`) VALUES ('ARPNOMODIFY', '1')");
+    view.Execute();
 
-        custact = (type & msidbCustomActionTypeInScript) ? "custact" : "normal";
-        noimp = (type & msidbCustomActionTypeNoImpersonate) ? "noimp" : "imp";
+    view = database.OpenView("INSERT INTO `Property` (`Property`.`Property`, `Property`.`Value`) VALUES ('ARPNOREPAIR', '1')");
+    view.Execute();
 
-        if ((type & msidbCustomActionTypeInScript) | 1) {
-            WScript.StdOut.WriteLine(WScript.ScriptName + ": data: " + action + "|" + type.toString(16) + "|" + custact+"|" + noimp);
-        }
-
-        record = view.Fetch();
-        }
-
-	view.Close();
+    view.Close();
 	database.Commit();
     }
 catch(e)
     {
-	WScript.StdErr.WriteLine(e);
+	WScript.StdErr.WriteLine("exception thrown: " + e);
 	WScript.Quit(1);
     }
 
